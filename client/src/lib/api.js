c7c28fd246ddcead614dd8cfcce922d934eef3df
Kalyani Dhananjayan <kalyanidhananjayan2005@gmail.com>
2025-12-26 19:25:54 +0530

    fix deployment bugs

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://holy-saint-backend.onrender.com';
 
 const fetchAPI = async (endpoint, options = {}) => {
   const response = await fetch(`${API_URL}${endpoint}`, {
